e55e499f6ab9d56366d71f3a6248b57a2b041b4e
Soly Blacutt <solange.blacutt@ing.austral.edu.ar>
2025-11-11 19:28:59 +0100

    fix(router): hash routing in wouter for GH Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,25 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
+import { Router as WouterRouter, Switch, Route } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
 import NotFound from "@/pages/not-found";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        {/* Hash routing para que funcione perfecto en GitHub Pages */}
+        <WouterRouter hook={useHashLocation}>
+          <Switch>
+            <Route path="/" component={Home} />
+            {/* Ruta por defecto (404 / Not Found) */}
+            <Route component={NotFound} />
+          </Switch>
+        </WouterRouter>
       </TooltipProvider>
     </QueryClientProvider>
   );
