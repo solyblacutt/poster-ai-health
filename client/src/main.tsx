1d25334c108c8b775c6b20f28ca6e3784178c090
Soly Blacutt <solange.blacutt@ing.austral.edu.ar>
2025-11-11 19:29:46 +0100

    fix(router): hash routing in wouter for GH Pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
