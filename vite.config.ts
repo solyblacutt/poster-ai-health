2f1ee27a454569f078a19530e4828cda4934cede
Soly Blacutt <solange.blacutt@ing.austral.edu.ar>
2025-11-11 19:14:06 +0100

    assets folder

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// Vite construirá tomando como raíz la carpeta "client"
-// y dejará los archivos finales en "server/public" (que tu Express ya sabe servir).
-// Para GitHub Pages, subiremos ese "server/public" como artefacto estático.
-
 export default defineConfig({
-  base: "/poster-ai-health/", // 👈 nombre EXACTO de tu repo en GitHub
+  base: "/poster-ai-health/",
   root: path.resolve(__dirname, "client"),
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "client", "src"), // 👈 alias "@"
+    },
+  },
   build: {
-    outDir: path.resolve(__dirname, "server/public"),
+    outDir: path.resolve(__dirname, "server", "public"),
     emptyOutDir: true,
   },
 });
+
